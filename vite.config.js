40418f18703bb6b24e59f794726da3ce2f81e137
Alexander Groth <alexander@grothsoftwaresolutions.no>
2025-08-13 19:55:23 +0200

    Add partials to make maintenance easier

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,20 @@
-import { defineConfig } from 'vite'
-import { resolve } from 'path'
+import { defineConfig } from "vite";
+import { resolve } from "path";
+import handlebars from "vite-plugin-handlebars";
 
 export default defineConfig({
-    build: {
-        rollupOptions: {
-            input: {
-                main: resolve(__dirname, 'index.html'),
-                skills: resolve(__dirname, 'skills.html'),
-                process: resolve(__dirname, 'process.html')
-            }
-        }
-    }
-})
+  plugins: [
+    handlebars({
+      partialDirectory: resolve(__dirname, "partials"),
+    }),
+  ],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        skills: resolve(__dirname, "skills.html"),
+        process: resolve(__dirname, "process.html"),
+      },
+    },
+  },
+});
